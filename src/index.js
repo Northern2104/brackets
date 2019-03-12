06a06442dcc51d2962926dbe3950215ef2778e27
Northern2104 <shinigami210486@gmail.com>
2019-03-12 18:12:14 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  	let br=[];
+	let brSame= [];
+	let flagg=true;
+	for(i=0;i<str.length;i++){
+		bracketsConfig.forEach(function callback(arr){
+			if(arr[0]!=arr[1]){
+				if(str[i]==arr[0]){
+					br.push(str[i])
+				}
+
+				else if(str[i]==arr[1]){
+					if(br.length>0&&br[br.length-1]==arr[0]){
+					br.pop();
+					}
+					else{flagg=false};
+				}
+			}
+			else{
+				if(str[i]==arr[0]&&brSame.length==0){
+					brSame.push(str[i]);
+					br.push(str[i]);
+				}
+
+				else if(str[i]==arr[0]&&brSame.length>0){
+					brSame.pop();
+					br.pop();
+				}
+			}
+		})
+	};
+if(br.length>0||brSame.length>0){flagg=false};
+return flagg;
 }
